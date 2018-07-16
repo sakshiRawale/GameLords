15de2469b9c542d405239849bb9bed7b3b9ca92f
Sakshi Rawale <sakshi.jrawale@gmail.com>
2018-07-16 17:10:34 +0530

    add splash screen changes

--- android/app/src/main/java/com/gamelords/MainApplication.java
+++ android/app/src/main/java/com/gamelords/MainApplication.java
@@ -3,9 +3,9 @@ package com.gamelords;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactnativepayments.ReactNativePaymentsPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -32,9 +32,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SplashScreenReactPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage(),
+            new SplashScreenReactPackage(),
             new ReactNativePaymentsPackage(),
             new OrientationPackage(),
             new LinearGradientPackage(),
