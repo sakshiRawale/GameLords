0ac9624876dea919149ef430108180e82932ec43
Sakshi Rawale <sakshi.jrawale@gmail.com>
2018-07-31 17:26:54 +0530

    fix api url

--- app/constants/api.js
+++ app/constants/api.js
@@ -8,14 +8,14 @@
 {/* Game Lord */}
 
 {/* Game Lord */}
-const hostGL = "http://my.game-lords.com";
+const hostGL = "http://play.game-lords.com";
 const portGL = "3003";
 export const BASE_API_URL_GL =  `${hostGL}:${portGL}/api`;
 export const BASE_URL_GL =  `${hostGL}:${portGL}/`;
 
 {/* Profile Pic */}
 
-const hostPP = "http://my.game-lords.com";
+const hostPP = "http://play.game-lords.com";
 const portPP = "3003";
 export const BASE_API_URL_PP =  `${hostPP}:${portPP}/api`;
 export const BASE_URL_PP =  `${hostPP}:${portPP}/`;
